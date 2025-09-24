89b2e6e13fb2b200e56eec619ae1a331f6a57f5b
admin <gederuio349@gmail.com>
2025-09-25 01:00:51 +0500

    add read function to RAMAgent

--- agents.py
+++ agents.py
@@ -1,4 +1,17 @@
+import os
+from openai import OpenAI
+import json 
 
+def load_openai_api_key(file_path="LLM_as_computer/config.txt"):
+    with open(file_path, "r") as f:
+        for line in f:
+            if line.startswith("OPENAI_API_KEY ="):
+                return line.split("=", 1)[1].strip().strip("'").strip('"')
+    return None
+
+OPENAI_API_KEY = load_openai_api_key()
+
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 # пример функции openai
 """
 client = OpenAI()
@@ -66,11 +79,55 @@ class SimpleAgent(Agent):
 
 
 class RAMAgent(Agent):
-    def __init__(self, name: str = "RAMAgent", model: str = "default_model"):
+    def __init__(self, name: str = "RAMAgent", model: str = "gpt-5-nano"):
         super().__init__(name, model)
+        self.ram_storage = [9, 3, 4, 5, 6, 0, 10, 25, 678]
 
-    def run(self, input_text: str) -> str:
-        if input_text == 'READ':
-            data_bus.value = self.memory[addr]
-        elif input_text == 'WRITE'
-        return
+    def read(self, need_index: int) -> int:
+        client = OpenAI()
+        functions = [
+                {
+                    "name": "ram_read",
+                    "description": "Реализация метода READ в RAM",
+                    "parameters": {
+                        "type": "object",
+                        "properties": {
+                            "value": {
+                                "type": "integer",
+                                "description": "Числовое значение"
+                            }
+                        },
+                        "required": ["value"]
+                    }
+                }
+            ]
+
+        response = client.chat.completions.create(
+                model=self.model,
+                messages=[
+                    {
+                        "role": "system",
+                        "content": "Ты модуль в ОЗУ компьютера, который отвечает за операцию READ. "
+                                    "Ты получаешь хранилище ОЗУ(массив чисел) и числовой индекс. Ты должен вернуть число из хранилища которое соответсвует данному индексу(нумерация в хранилище начинается с единицы)."
+                                    "Пример входных данных: (storage: [0, 9, 5, 9, 0, 3], index: 3) -> ожидаемый вывод: 5 "
+                                    "Если запрошенный индекс не входит в хранилище, или произошла другая проблема тебе необходимо вернуть значение -1" # потом нужно будет сменить логику ошибки
+                    },
+                    {
+                        "role": "user",
+                        "content": f"Хранилище: {self.ram_storage}, Запрошенный индекс: {need_index}"
+                    },
+                ],
+                functions=functions,
+                function_call={"name": "ram_read"},
+            )
+        arguments = response.choices[0].message.function_call.arguments
+        read_output = json.loads(arguments)
+        return read_output.get('value')
+
+
+    def write(self, need_index: int) -> str:
+        pass
+
+
+ram1 = RAMAgent()
+print(ram1.read(8))
